8dfbf634a25b0e8d8bcad47b2bcc85e770d753a7
Furkan Turgut <saianss68@gmail.com>
2020-04-04 23:48:01 +0300

    Update kimlikoop.py

--- kimlikoop.py
+++ kimlikoop.py
@@ -28,3 +28,51 @@ print(c1)
 
 k1 = Kimlik("Furkan","Turgut",2001,"Aksaray")
 print(k1)
+
+print( """
+[1]Kimlik Detaylı Görüntüle
+***************************
+[a]Kimlik Numarası
+[b]İsim
+[c]Soyisim
+[d]Doğum Yılı
+[e]Memleket
+
+[2]Ehliyet Detaylı Görüntüle
+****************************
+[f]Ehliyet Numarası
+[g]Aldığı Yıl
+""")
+while True:
+    os.system("cls")
+    mainmenü =(input("Seçinizi Giriniz:"))
+
+    if mainmenü =="1":
+        print(kimlik)
+        time.sleep(3)
+    elif mainmenü =="a" :
+        print(" T.C Kimlik Numarası:",kimlik.tcNo)
+        time.sleep(3)
+    elif mainmenü =="b":
+        print("İsim:",kimlik.isim)
+        time.sleep(3)
+    elif mainmenü =="c":
+        print("Soyad:",kimlik.soyad)
+        time.sleep(3)
+    elif mainmenü =="d":
+        print("Doğum Yılı:",kimlik.dogumYili)
+        time.sleep(3)
+    elif mainmenü =="e":
+        print("Memleket:",kimlik.memleket)
+        time.sleep(3)
+    elif mainmenü =="2":
+        print(ehliyet)
+        time.sleep(3)
+    elif mainmenü =="f":
+        print("Ehliyet Numarası:",ehliyet.ehliyetNo)
+        time.sleep(3)
+    elif mainmenü =="g":
+        print("Aldığı Yıl:",ehliyet.aldigiYil)
+        time.sleep(3)
+    else:
+        print("Hatalı Giriş!!!\nLütfen menüye göre seçim yapınız!")
